6ac2c2782d4f7631fdbc1f4dc1dffbb3196f37da
Grazvydas-M <m.grazvydas@yahoo.com>
2021-12-07 11:56:52 +0200

    Initial commit

---
+++ app/config/config.php
@@ -0,0 +1,16 @@
+<?php
+// DB params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '123456');
+define('DB_NAME', 'tmvc');
+// app root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL root
+define('URLROOT', 'http://localhost/personal-homework');
+
+// Site name
+define('SITENAME', 'Personal-homework');
+
+
